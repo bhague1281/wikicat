ccb58d085eb913af5162abb22439aec99968ce06
bhague1281 <bhague1281@gmail.com>
2016-04-06 11:05:18 -0700

    added serializer to application controller, and modified category controller

--- app/controllers/api/v1/category_controller.rb
+++ app/controllers/api/v1/category_controller.rb
@@ -1,6 +1,4 @@
 class Api::V1::CategoryController < ApplicationController
-  respond_to :json
-
   def show
     category = params[:category] ? params[:category] : "sports"
     @category = Category.find_by_cat_title(category)
