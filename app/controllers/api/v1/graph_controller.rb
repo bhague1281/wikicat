52fbc99ecdce34a1b91ac436558c0b532bb24ea0
bhague1281 <bhague1281@gmail.com>
2016-04-06 11:17:44 -0700

    setup graph endpoint displaying a category's subcategories

---
+++ app/controllers/api/v1/graph_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::GraphController < ApplicationController
+  def show
+    category = params[:category] || 'sports'
+    @category = Category.find_by_cat_title(category)
+    @links = Link.where(:cl_to => @category.cat_title,
+                        :cl_type => "subcat")
+    render :json => @links,
+    each_serializer: LinkSerializer,
+    root: @category.cat_title
+  end
+
+  private
+
+  def graph_params
+    params.require(:category).permit(:cl_to, :cl_type)
+  end
+end
