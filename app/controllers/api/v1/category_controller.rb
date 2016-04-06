668ac44c46b659d8b7451e548af8bcbeaeb3b7f5
bhague1281 <bhague1281@gmail.com>
2016-04-06 14:46:41 -0700

    added category helper and check for category in controller

--- app/controllers/api/v1/category_controller.rb
+++ app/controllers/api/v1/category_controller.rb
@@ -1,10 +1,19 @@
 class Api::V1::CategoryController < ApplicationController
   def show
-    category = params[:category] || "sports"
+    category = params[:category] || "Main_topic_classifications"
     @category = Category.find_by_cat_title(category)
 
-    render :json => @category,
-    serializer: CategorySerializer,
-    root: "category"
+    if @category
+      render :json => @category,
+      serializer: CategorySerializer,
+      root: "category"
+    else
+      render :json => {
+        :error => {
+          :text => "404 not found",
+          :status => 404
+        }
+      }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'category/:category' => 'category#show', :as => :category
+      get 'category' => 'category#show', :as => :category
+      get 'category/:category' => 'category#show', :as => :category_show
       get 'graph' => 'graph#show', :as => :graph
       get 'graph/:category' => 'graph#show', :as => :graph_show
     end
