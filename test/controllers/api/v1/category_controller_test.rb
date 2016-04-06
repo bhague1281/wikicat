d4f6b1bfb0f8ca91c9dcac74b3b55b22ba50de86
bhague1281 <bhague1281@gmail.com>
2016-04-06 12:03:14 -0700

    altered tests to convert hashes to json, for readability

--- test/controllers/api/v1/category_controller_test.rb
+++ test/controllers/api/v1/category_controller_test.rb
@@ -2,9 +2,16 @@ require 'test_helper'
 
 class Api::V1::CategoryControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
+    result = {
+      :category => {
+        :title => "science",
+        :sub_categories => 1
+      }
+    }
+
     get api_v1_category_url "science"
     assert_response :success
-    assert_equal "{\"category\":{\"title\":\"science\",\"sub_categories\":1}}",
+    assert_equal result.to_json,
                  @response.body
   end
 end
--- test/controllers/api/v1/graph_controller_test.rb
+++ test/controllers/api/v1/graph_controller_test.rb
@@ -2,9 +2,15 @@ require 'test_helper'
 
 class Api::V1::GraphControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
+    result = {
+      :sports => [
+        :sub_category => "culture_and_sports_culture"
+      ]
+    }
+
     get api_v1_graph_url "sports"
     assert_response :success
-    assert_equal "{\"sports\":[{\"sub_category\":\"culture_and_sports_culture\"}]}",
+    assert_equal result.to_json,
                  @response.body
   end
 
