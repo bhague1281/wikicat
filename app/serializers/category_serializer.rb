c4078fee646ddc1cbf6e4387efa1ce3b7f3320fd
bhague1281 <bhague1281@gmail.com>
2016-04-06 14:45:53 -0700

    fixed serializer encodings, added links to categories

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,12 +1,27 @@
 class CategorySerializer < ActiveModel::Serializer
-  attributes :title, :sub_categories
+  attributes :title, :sub_categories, :_links
 
   def title
-    URI::encode(object.cat_title.force_encoding("ISO-8859-1"))
-        .encode("utf-8", replace: nil).downcase.gsub("%20", "_")
+    URI::encode(object.cl_sortkey
+        .encode('ASCII', {replace: ''})
+        .gsub(/\s+/, ' ').strip.downcase.tr(" ", "_"))
   end
 
   def sub_categories
     object.cat_subcats
+  end
+
+  def _links
+    {:self => _self, :graph => _graph}
+  end
+
+  def _graph
+    href = URI::encode("/api/v1/graph/#{self.title}")
+    {:href => href, :method => "GET", :rel => "graph"}
+  end
+
+  def _self
+    href = URI::encode("/api/v1/category/#{self.title}")
+    {:href => href, :method => "GET", :rel => "self"}
   end
 end
